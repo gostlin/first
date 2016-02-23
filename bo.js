b9570ba1495c0671fbb6a73630a2df297662f078
gostlin <454094941@qq.com>
2016-02-23 17:27:13 +0800

    complain

--- bo.js
+++ bo.js
@@ -1,1 +1,19 @@
-adwdaw
+function drag(id){
+	var obj=document.getElementById(id)
+	var offX=0;
+	var offY=0;
+	
+	obj.onmousedown=function(e){
+		offX=e.pageX-obj.offsetLeft;
+		offY=e.pageY-obj.offsetTop;
+		document.onmousemove=function(e){
+			obj.style.left=e.pageX-offX+'px';
+			obj.style.top=e.pageY-offY+'px';
+		}
+		document.onmouseup=function(){
+			document.onmousemove=null;
+			document.onmouseup=null;
+			
+		}
+	}
+}
